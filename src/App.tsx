c64e70ef030ee822e76adaa196eadfd5e01a0eae
shafucheng <flynn.sha@outlook.com>
2021-10-30 17:00:20 +0800

    feat: layout and menu collapsed

--- src/App.tsx
+++ src/App.tsx
@@ -23,23 +23,30 @@ const App: FC = () => {
   autoTheme.addEventListener('change', setTheme)
   useEffect(setTheme)
 
+  const [collapsed, setCollapsed] = useState(false)
+
   return (
     <ThemeContext.Provider value={{ themeMode, setThemeMode }}>
-      <div className="h-screen bg-white dark:bg-dark-900 text-black dark:text-white text-opacity-85 dark:text-opacity-85">
-        <ThemeContext.Consumer>{(value) => {
-          switch(value.themeMode) {
-            case ThemeMode.auto:
-              return '自动'
-            case ThemeMode.light:
-              return '浅色'
-            case ThemeMode.dark:
-              return '深色'
-          }
-        }}</ThemeContext.Consumer>
-        <br />
-        <button onClick={() => setThemeMode(ThemeMode.auto)}>自动</button>
-        <button onClick={() => setThemeMode(ThemeMode.light)}>浅色</button>
-        <button onClick={() => setThemeMode(ThemeMode.dark)}>深色</button>
+      <div className="h-screen bg-white dark:bg-dark-900 text-black dark:text-white text-opacity-85 dark:text-opacity-85 flex">
+        <div className={`box-content border-r border-r-light-500 dark:border-r-dark-500 transition-width ${collapsed ? 'w-12' : 'w-60'}`}>
+          <button onClick={() => setCollapsed(!collapsed)}>切换</button>
+          <br />
+          <ThemeContext.Consumer>{(value) => {
+            switch(value.themeMode) {
+              case ThemeMode.auto:
+                return '自动'
+              case ThemeMode.light:
+                return '浅色'
+              case ThemeMode.dark:
+                return '深色'
+            }
+          }}</ThemeContext.Consumer>
+        </div>
+        <div className="flex-1">
+          <button onClick={() => setThemeMode(ThemeMode.auto)}>自动</button>
+          <button onClick={() => setThemeMode(ThemeMode.light)}>浅色</button>
+          <button onClick={() => setThemeMode(ThemeMode.dark)}>深色</button>
+        </div>
       </div>
     </ThemeContext.Provider>
   )
